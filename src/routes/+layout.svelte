4a374d09e59e68fd63e29a32cd3fd3f3274d6618
baglana <102351104+baglana@users.noreply.github.com>
2024-01-22 20:52:35 +0600

    Add css reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import 'reset-css';
 	import Header from '$lib/Header.svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 </script>
 
-<Header planets={data.planets}/>
+<Header planets={data.planets} />
 
 <main>
 	<slot />
